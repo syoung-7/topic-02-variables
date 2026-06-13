8192ec2d429331a7f872a8c0c52affe7546135c8
syoung-7 <syoung7@edisonohio.edu>
2026-06-13 19:19:20 -0400

    Create topic-02-solution.py
    Add practice solution

---
+++ topic-02-solution.py
@@ -0,0 +1,22 @@
+# Topic 02 Collaborative Assignment
+# Your Name:Steve Young  
+# Date:6-13-2026
+
+# --- STARTER CODE ---
+# Step 1: Store some information about yourself
+name = "Steve Young"           # Replace with your name
+favorite_number = 15  # Replace with your favorite number
+hobby = bowling
+
+# Step 2: Write an expression using your variables
+# Example: double = favorite_number * 2
+year = 2026
+birth_year = year - age
+# Step 3: Print a summary
+# Example: print("My name is", name)
+
+# --- YOUR CODE BELOW THIS LINE ---
+print("My name is", name)
+print("I am", age, "years old and I enjoy", hobby)
+print("My favorite number is", number)
+print("Since we are in year", year, "I was born in", birth_year)
